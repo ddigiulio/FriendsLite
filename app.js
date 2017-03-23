13d84fbd05a378942498e2d87630bd63bd4d7c68
ddigiulio <ddigiulio90@gmail.com>
2017-03-23 16:33:36 -0700

    quick change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $( window ).on( "load", function(e) {
-    console.log(e);
-   // console.log("HERE");
+  //  console.log(e);
+   console.log("HERE");
  })
 
 //  $("#loginButton").on("click",function())
