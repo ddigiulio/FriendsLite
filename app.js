ec732379736838dc22bd9af9b32662556233d542
ddigiulio <ddigiulio90@gmail.com>
2017-03-23 22:11:21 -0700

    ad

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 $(window).on("load", function (e) {
   //console.log(e);
   //console.log("HERE");
-})
+});
 
 var ACCESSTOKEN = "4654737219.ad1ff53.feb5c1991ca64a4fa0b6579cefa873fd";
 var endPoint = "https://api.instagram.com/v1/users/self/?access_token=";
-
+var URL = endPoint + ACCESSTOKEN;
 console.log("HERE");
 
-function display() {
-  console.log("HERHERHE");
-  var URL = endPoint + ACCESSTOKEN;
-  $.get(URL, function (data) {
-    console.log("HERE");
-  });
-}
+$.get(URL, function(data) {
+console.log(data);
+});
