102310ba9b4cd32345583f448d9e05335da504bf
ddigiulio <ddigiulio90@gmail.com>
2017-03-23 21:23:45 -0700

    a

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 $( window ).on( "load", function(e) {
     console.log(e);
-   console.log("HERE");
+   //console.log("HERE");
  })
 
  var ACCESSTOKEN = "4654737219.ad1ff53.feb5c1991ca64a4fa0b6579cefa873fd";
- var getUsers = ( fuction(
 
- ))
+
 
 //  $("#loginButton").on("click",function())
