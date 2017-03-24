61e5baf9b6d1b7039c629a1106eaece04ae8e11e
ddigiulio <ddigiulio90@gmail.com>
2017-03-23 21:42:48 -0700

    a

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ $( window ).on( "load", function(e) {
  })
 
  var ACCESSTOKEN = "4654737219.ad1ff53.feb5c1991ca64a4fa0b6579cefa873fd";
+ var endPoint = "https://api.instagram.com/v1/users/self/?access_token=";
 
 
-
-//  $("#loginButton").on("click",function())
+ function display() {
+    var URL=endPoint + ACCESSTOKEN;
+    $.get(URL, function(data){
+      console.log(data);
+  });
+}
