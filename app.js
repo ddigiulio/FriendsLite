8662827a4f4c2c7a846edbc49ee1e2fabc3c430a
ddigiulio <ddigiulio90@gmail.com>
2017-03-23 21:53:41 -0700

    a

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-$( window ).on( "load", function(e) {
-    //console.log(e);
-   //console.log("HERE");
- })
+$(window).on("load", function (e) {
+  //console.log(e);
+  //console.log("HERE");
+})
 
- var ACCESSTOKEN = "4654737219.ad1ff53.feb5c1991ca64a4fa0b6579cefa873fd";
- var endPoint = "https://api.instagram.com/v1/users/self/?access_token=";
+var ACCESSTOKEN = "4654737219.ad1ff53.feb5c1991ca64a4fa0b6579cefa873fd";
+var endPoint = "https://api.instagram.com/v1/users/self/?access_token=";
 
+console.log("HERE");
 
- function display() {
-    var URL=endPoint + ACCESSTOKEN;
-    $.get(URL, function(data){
-      console.log("HERE");
+function display() {
+  var URL = endPoint + ACCESSTOKEN;
+  $.get(URL, function (data) {
+    console.log("HERE");
   });
 }
